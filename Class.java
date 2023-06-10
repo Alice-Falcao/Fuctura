59492150935f379159442ddd1ddc358675f0a76e
Alice-Falcao <136180824+Alice-Falcao@users.noreply.github.com>
2023-06-10 11:58:01 -0300

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,11 @@
+package Apresentacao;
+
+public class Class {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo, estou na Fuctura."
+				+ " A aula de hoje foi legal"); 
+		System.out.println
+	}
+
+}
